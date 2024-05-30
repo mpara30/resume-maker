93f917f2abc98ea499e8716e8c861920ead69028
mpara30 <para.mihai99@gmail.com>
2024-05-30 12:43:24 +0300

    Read data from JSON

--- src/main/java/com/resumemaker/ResumeMakerApplication.java
+++ src/main/java/com/resumemaker/ResumeMakerApplication.java
@@ -1,13 +1,22 @@
 package com.resumemaker;
 
+import com.resumemaker.service.ResumeGeneratorService;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResumeMakerApplication {
+public class ResumeMakerApplication implements CommandLineRunner {
+	@Resource
+	ResumeGeneratorService resumeGeneratorService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResumeMakerApplication.class, args);
+	}
+	@Override
+	public void run(String... args) {
+		resumeGeneratorService.generateResume();
 	}
 
 }
--- src/main/java/com/resumemaker/model/Data.java
+++ src/main/java/com/resumemaker/model/Data.java
@@ -3,13 +3,15 @@ package com.resumemaker.model;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @ToString
 public class Data {
-    PersonalData personalData;
-    Education education;
-    Work work;
-    Skills skills;
-    Projects projects;
-    Certificates certificates;
+    PersonalData personal_data;
+    List<Education> education;
+    List<Work> work;
+    List<Skills> skills;
+    List<Projects> projects;
+    List<Certificates> certificates;
 }
